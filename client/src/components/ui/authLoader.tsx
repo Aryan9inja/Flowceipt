df4dde4eb4e2dfc218c7f6ab884d586879b5596b
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-08-15 22:58:30 +0530

    Add authLoader for loading during authState loading

---
+++ client/src/components/ui/authLoader.tsx
@@ -0,0 +1,7 @@
+export default function AuthLoader() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-bg">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-primary"></div>
+    </div>
+  );
+}
