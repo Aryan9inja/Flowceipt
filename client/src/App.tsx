7592402716432f2ea25d8c2bb03b038b87e6f130
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-08-15 22:59:37 +0530

    Add check for auth on app start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
 import AppRoutes from "./routes/routes";
+import type { AppDispatch, RootState } from "./store/store";
+import { useEffect } from "react";
+import { getUserThunk } from "./store/thunks/authThunk";
+import AuthLoader from "./components/ui/authLoader";
 
 function App() {
-  return <AppRoutes/>
+  const dispatch: AppDispatch = useDispatch();
+  const isAuthLoading = useSelector(
+    (state: RootState) => state.auth.isAuthLoading
+  );
+
+  useEffect(() => {
+    dispatch(getUserThunk());
+  }, [dispatch]);
+
+  if (isAuthLoading) return <AuthLoader/>;
+  return <AppRoutes />;
 }
 
 export default App;
