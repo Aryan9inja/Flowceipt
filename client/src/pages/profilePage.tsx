58bd94d421b494d42c161935d45f8efa91bb7759
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-10-03 14:20:00 +0530

    Refactor ProfilePage layout for improved structure and styling

--- client/src/pages/profilePage.tsx
+++ client/src/pages/profilePage.tsx
@@ -5,21 +5,22 @@ import ActivityPanel from "../components/profile/activityPanel";
 
 const ProfilePage = () => {
   return (
-    <div className="flex min-h-screen">
+    <div className="min-h-screen bg-gradient-to-br from-bg to-card/80 flex">
       {/* Desktop sidebar */}
-      <div className="hidden sm:flex">
+      <div className="hidden sm:block">
         <DesktopNav />
       </div>
 
       {/* Main content */}
-      <div className="w-full bg-bg sm:ml-64">
-        <ProfileInfo />
-        <ActivityPanel />
-      </div>
-
-      {/* Mobile bottom nav */}
-      <div className="sm:hidden  w-full fixed bottom-0 left-0 z-50">
-        <MobileNav />
+      <div className="flex-1 flex flex-col items-center sm:ml-64 mb-18 sm:mb-0">
+        <main className="w-full max-w-4xl px-4 py-8 flex flex-col gap-8">
+          <ProfileInfo />
+          <ActivityPanel />
+        </main>
+        {/* Mobile bottom nav */}
+        <div className="sm:hidden w-full fixed bottom-0 left-0 z-50">
+          <MobileNav />
+        </div>
       </div>
     </div>
   );
